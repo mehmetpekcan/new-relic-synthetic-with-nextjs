469cc4202b1d630b0417ea57149cf489cdd4a354
mehmetpekcan <pekcanmehmett@gmail.com>
2021-12-10 22:08:45 +0300

    Add api and pages for NewRelic tests

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <div>NextJS App</div>;
+import React from "react";
+
+function IndexPage() {
+  return (
+    <div>
+      <h1>Index Page</h1>
+    </div>
+  );
 }
+
+export default IndexPage;
