b74226623b18a195bf8841903100e5ba6a1a2808
mehmetpekcan <pekcanmehmett@gmail.com>
2021-12-10 23:52:22 +0300

    Make NewRelic browser integration consistent

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,18 @@
 import Head from "next/head";
 
-import { NewRelicSnippet } from "../modules/newrelic-browser-agent";
+import NewRelic from "../components/NewRelic";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
         <title>NewRelic Synthetic Playground with NextJs</title>
-        {process.env.NODE_ENV === "production" && <NewRelicSnippet />}
+        {process.env.NODE_ENV === "production" && (
+          <NewRelic
+            licenseKey={process.env.NEW_RELIC_BROWSER_LICENCE_KEY}
+            applicationID={process.env.NEW_RELIC_APP_ID}
+          />
+        )}
       </Head>
       <Component {...pageProps} />
     </>
