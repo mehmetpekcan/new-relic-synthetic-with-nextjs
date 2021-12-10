d13d070249eaff6e7f1a0e1396901ed91d7ed531
mehmetpekcan <pekcanmehmett@gmail.com>
2021-12-10 22:01:46 +0300

    NewRelic installation has been done and verified

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
+if (process.env.NODE_ENV === "production") {
+  try {
+    console.log("NewRelic is starting...");
+    require("newrelic");
+  } catch (error) {
+    console.log("There happened an error during NewRelic starting", error);
+  }
+}
+
 module.exports = {
   reactStrictMode: true,
-}
+};
