ae19dbf9fbc57f376ab659b03b5d54e35f0ba0bd
mehmetpekcan <pekcanmehmett@gmail.com>
2021-12-11 00:09:24 +0300

    Revert back prod env only

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
-require("newrelic");
+if (process.env.NODE_ENV === "production") {
+  try {
+    console.log("NewRelic is starting...");
+    require("newrelic");
+  } catch (error) {
+    console.log("There happened an error during NewRelic starting", error);
+  }
+}
 
 module.exports = {
   reactStrictMode: true,
