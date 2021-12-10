b7ad5f6b31b130789b78ab4ba1b20a5c85e7d0e7
mehmetpekcan <pekcanmehmett@gmail.com>
2021-12-11 00:04:36 +0300

    Try new relic for every env

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
-if (process.env.NODE_ENV === "production") {
-  try {
-    console.log("NewRelic is starting...");
-    require("newrelic");
-  } catch (error) {
-    console.log("There happened an error during NewRelic starting", error);
-  }
-}
+require("newrelic");
 
 module.exports = {
   reactStrictMode: true,
